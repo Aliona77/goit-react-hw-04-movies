be8bdcd5e432899cded1835d9a496551ab109d86
Aliona77 <chabanalona153@gmail.com>
2021-08-02 13:37:43 +0300

    netlify

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,6 @@ import PageHeading from '../components/PageHeading/PageHeading';
 import { Link } from 'react-router-dom';
 import { fetchMovieTrending } from '../servises/moviesApi';
 import styles from '../components/MovieList/MovieList.module.css';
-//import Spinner from '../components/Spiner/Spiner';
 
 const imageSrc = 'https://image.tmdb.org/t/p/w500';
 
